71e545be81a38bc828d6286adbe95edb4f88e442
Ajay <ajaydeepak07@gmail.com>
2019-01-18 23:29:39 +0530

    DNA - RNA

---
+++ src/main/kotlin/DNAtoRNA.kt
@@ -0,0 +1,23 @@
+import java.lang.IllegalArgumentException
+
+/**
+ * @Author Ajay on 18-01-2019.
+ */
+
+fun transcibeToRNA(dna: String): String =
+        dna.fold(""){
+            acc, nucleotide ->
+            acc + when(nucleotide){
+                'G' -> println("G -> C")
+                'C' -> 'G'
+                'T' -> 'A'
+                'A' -> 'U'
+                else -> throw IllegalArgumentException("Given DNA characters not present in {G,C,T,A}")
+            }
+}
+
+
+fun main(args: Array<String>) {
+    transcibeToRNA("GCTA")
+
+}
---
+++ src/test/kotlin/TransciptTest.kt
@@ -0,0 +1,13 @@
+import org.junit.Test
+import kotlin.test
+
+/**
+ * @Author Ajay on 18-01-2019.
+ */
+class TransciptTest {
+
+    @Test
+    fun rnaTranscript(){
+        assertEquals()
+    }
+}
