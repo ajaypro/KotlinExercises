2ba97ed6461125edfdfa92292d6bbb65b0374886
Ajay <ajaydeepak07@gmail.com>
2019-01-20 14:25:07 +0530

    Test cases

--- src/test/kotlin/TransciptTest.kt
+++ src/test/kotlin/TransciptTest.kt
@@ -8,6 +8,26 @@ import kotlin.test.assertEquals
 class TransciptTest {
 
     @Test
+    fun cytosineComplementIsGuanine() {
+        assertEquals("G", transcibeToRNA("C"))
+    }
+
+    @Test
+    fun guanineComplementIsCytosine() {
+        assertEquals("C", transcibeToRNA("G"))
+    }
+
+    @Test
+    fun thymineComplementIsAdenine() {
+        assertEquals("A", transcibeToRNA("T"))
+    }
+
+    @Test
+    fun adenineComplementIsUracil() {
+        assertEquals("U", transcibeToRNA("A"))
+    }
+
+    @Test
     fun rnaTranscript(){
         assertEquals("GCTACTGCA", transcibeToRNA("CGAUGACGU"))
     }
