79e1ae66a9a5ebe4a9d71742ecdef2c440d34c42
Ajay <ajaydeepak07@gmail.com>
2019-01-28 18:39:30 +0530

    space age addition

--- src/main/kotlin/spaceage/SpaceAge.kt
+++ src/main/kotlin/spaceage/SpaceAge.kt
@@ -1,6 +1,7 @@
 package spaceage
 
 import spaceage.Planet.*
+import java.math.BigDecimal
 
 /**
  * @Author Ajay on 27-01-2019.
@@ -9,9 +10,30 @@ data class SpaceAge(val ageInSeconds: Int) {
     var seconds = ageInSeconds
 
     fun spaceAge(planet: Planet):Double =
-        ageInSeconds/planet.orbitalPeriod
+        secondsToYear( ageInSeconds/planet.orbitalPeriod).roundedTo(2)
 
     fun onEarth() = spaceAge(EARTH)
+    fun onMercury() = spaceAge(MERCURY)
+    fun onVenus()   = spaceAge(VENUS)
+    fun onMars()    = spaceAge(MARS)
+    fun onJupiter() = spaceAge(JUPITER)
+    fun onSaturn()  = spaceAge(SATURN)
+    fun onUranus()  = spaceAge(URANUS)
+    fun onNeptune() = spaceAge(NEPTUNE)
+
+
+
+    companion object TimeUtils{
+        private fun toMin(s: Double) = s/60.0
+        private fun toHour(min: Double) = min/60.0
+        private fun toDay(hr: Double) = hr/24
+        private fun toYear(day: Double) = day/365.25
+
+        fun secondsToYear(s:Double): Double = toYear(toDay(toHour(toMin(s))))
+
+        fun Double.roundedTo(decimal: Int): Double  =
+            BigDecimal(this).setScale(decimal, BigDecimal.ROUND_HALF_UP).toDouble()
+    }
 
 
 }
--- src/test/kotlin/SpaceAgeTest.kt
+++ src/test/kotlin/SpaceAgeTest.kt
@@ -10,6 +10,34 @@ class SpaceAgeTest {
     @Test
     fun ageInSeconds() {
         val age = SpaceAge(100000)
-        assertEquals(31.69, age.onEarth())
+        assertEquals(100000, age.seconds)
+    }
+
+    @Test
+    fun ageOnEarth(){
+        val age = SpaceAge(100000000)
+        assertEquals(31.69,age.onEarth())
+    }
+
+    @Test
+    fun ageOnMercury(){
+        val age = SpaceAge(231432123)
+        assertEquals(280.88,age.onMercury())
+    }
+
+    @Test
+    fun ageOnVenus() {
+        val age = SpaceAge(189839836)
+
+        assertEquals(6.02, age.onEarth())
+        assertEquals(9.78, age.onVenus())
+    }
+
+    @Test
+    fun ageOnMars() {
+        val age = SpaceAge(232987123)
+
+        assertEquals(73.83, age.onEarth())
+        assertEquals(39.25, age.onMars())
     }
 }
