92ab7e28c0420cc6b2ac8b34ad5eb4b4838c0250
Ajay <ajaydeepak07@gmail.com>
2019-02-09 22:04:21 +0530

    tests

--- src/test/kotlin/sumofmultiplestest.kt
+++ src/test/kotlin/sumofmultiplestest.kt
@@ -10,4 +10,14 @@ class sumofmultiplestest {
     fun `multiples of 3 or 5 up to 1`() {
         assertEquals(0, sumOfMultiples.sum(setOf(3, 5), 1))
     }
+
+    @Test
+    fun `multiples of 3 or 5 up to 4`() {
+        assertEquals(3, sumOfMultiples.sum(setOf(3, 5), 4))
+    }
+
+    @Test
+    fun `multiples of 3 or 5 up to 10`() {
+        assertEquals(23, sumOfMultiples.sum(setOf(3, 5), 10))
+    }
 }
