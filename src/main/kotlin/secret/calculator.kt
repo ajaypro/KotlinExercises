1942b8f5166afb7d512667521292176cbdd464e9
Ajay <ajaydeepak07@gmail.com>
2019-02-13 21:24:52 +0530

    doc for secret handshake

--- src/main/kotlin/secret/calculator.kt
+++ src/main/kotlin/secret/calculator.kt
@@ -3,6 +3,12 @@ package secret
 /**
  * @Author Ajay on 06-02-2019.
  */
+
+/**
+ * Given the input 3, the function would return the array ["wink", "double blink"] because 3 is 11 in binary.
+Given the input 19, the function would return the array ["double blink", "wink"] because 19 is 10011 in binary.
+Notice that the addition of 16 (10000 in binary) has caused the array to be reversed.
+ */
 object calculator {
 
     private val REVERSE_BIT_FLAG = 0b0001_0000
