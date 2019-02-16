2f51c3a299b21633aced29df3f5633dd1149029c
Ajay <ajaydeepak07@gmail.com>
2019-02-16 23:41:38 +0530

    doc for gigasecond

--- src/main/kotlin/Gigasecond.kt
+++ src/main/kotlin/Gigasecond.kt
@@ -2,6 +2,9 @@ import java.time.LocalDateTime
 
 /**
  * @Author Ajay on 25-01-2019.
+ *
+ * Calculate the moment when someone has lived for 10^9 seconds.
+A gigasecond is 10^9 (1,000,000,000) seconds.
  */
 class Gigasecond {
 
