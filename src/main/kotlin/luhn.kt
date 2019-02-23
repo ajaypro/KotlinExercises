cc77f94e98b3836466db393c2dfd9fa511edabce
Ajay <ajaydeepak07@gmail.com>
2019-02-23 22:39:03 +0530

    Luhn

--- src/main/kotlin/luhn.kt
+++ src/main/kotlin/luhn.kt
@@ -6,10 +6,25 @@
 object Luhn {
     fun isValid(input: String): Boolean {
         val stripped = input.filterNot { it.isWhitespace() }
-        if(stripped.length <= 1 || stripped.any { !it.isDigit() }) {
+        if (stripped.length <= 1 || stripped.any { !it.isDigit() }) {
             return false
         }
-
-        
+        return stripped.map { it.toString().toInt() }
+            .foldRight(emptyList<Int>()) { n, acc ->
+                /* if the accumulator size is odd, this is an even-indexed element (from right) */
+                if (acc.size % 2 == 1) acc + doubleAndReduce(n)
+                else acc + n
+            }.sum()
+            .isMultiplesOf(10)
     }
-}
+
+    private fun doubleAndReduce(n: Int) =
+        if ((n * 2) > 9) {
+            (n * 2) - 9
+        } else {
+            (n * 2)
+        }
+    }
+
+
+fun Int.isMultiplesOf(num: Int) = this % num == 0
